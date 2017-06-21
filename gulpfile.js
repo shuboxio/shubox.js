8969e98f333fb9bddddfa49ec27475fabeee16df
Joel Oliveira <joel.oliveira@gmail.com>
2017-06-20 23:38:45 -0400

    small is ultimately unsuccessful

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,23 @@ const ts = require('gulp-typescript')
 const mocha = require('gulp-mocha')
 const tsProject = ts.createProject('./tsconfig.json')
 const runSequence = require('run-sequence')
-const browserify = require('gulp-browserify')
-const rename = require('gulp-rename')
+const small = require('small').gulp
 
 gulp.task('bundle', function () {
-  return gulp.src('./src/shubox.js', { read: false })
-    .pipe(browserify({ standalone: 'Shubox' }))
-    .pipe(rename('bundle.js'))
-    .pipe(gulp.dest('dist'))
+  return gulp.src('./src/**/*.ts')
+    .pipe(tsProject()) // Compile typescript to javascript
+    .pipe(small('shubox.js', {
+      outputFileName: {
+        commonjs: 'output.common.js',
+        amd: 'output.amd.js',
+        standalone: 'output.standalone.js',
+        universal: 'output.universal.js'
+      },
+      exportPackage: {
+        universal: 'kek'
+      }
+    }))
+    .pipe(gulp.dest('./dist'))
 })
 
 gulp.task('build', function () {
