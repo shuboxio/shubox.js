d530cd05dc81f8b8eec71473265e724952554f37
Joel Oliveira <joel.oliveira@gmail.com>
2018-05-08 11:13:42 -0400

    Examples package

---
+++ packages/@shubox/examples/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './index.js',
+  },
+
+  output: {
+    filename: '[name].js',
+  },
+
+  mode: 'development',
+
+  devtool: 'inline-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [{loader: 'ts-loader'}],
+      },
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{loader: 'babel-loader'}],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js'],
+    modules: ['src', 'node_modules'],
+  },
+};
